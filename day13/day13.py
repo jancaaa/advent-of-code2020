76acb2bb24cf2ea4090db2f805383201206ddc20
Jana <janca.zahradnickova@gmail.com>
2020-12-14 19:37:34 +0100

    day13 part2 CRT solution

--- day13/day13.py
+++ day13/day13.py
@@ -39,8 +39,56 @@ def part2_generate_formula(departures: list) -> str:
     return formula
 
 
+def inv(a, m):
+    m0 = m
+    x0 = 0
+    x1 = 1
+
+    if (m == 1):
+        return 0
+
+    # Apply extended Euclid Algorithm
+    while (a > 1):
+        # q is quotient
+        q = a // m
+
+        t = m
+
+        # m is remainder now, process
+        # same as euclid's algo
+        m = a % m
+        a = t
+
+        t = x0
+
+        x0 = x1 - q * x0
+
+        x1 = t
+
+        # Make x1 positive
+    if (x1 < 0):
+        x1 = x1 + m0
+
+    return x1
+
+
 def part2(departures: list) -> int:
-    return
+    """
+    Solution using Chinese remainder theorem (https://en.wikipedia.org/wiki/Chinese_remainder_theorem)
+    """
+    prod = 1
+    for i, d in enumerate(departures):
+        if d != "x":
+            prod *= d
+
+    total = 0
+
+    for i, d in enumerate(departures):
+        if d != "x":
+            pp = prod // d
+            total += (d - i) * pp * pow(pp, d - 2, d)
+            total %= prod
+    return total
 
 
 if __name__ == "__main__":
